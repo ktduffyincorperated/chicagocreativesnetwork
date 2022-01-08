d0f6d977a708ccac74a01d09321a3009a5281b65
KT Duffy <ktduffyinc@gmail.com>
2022-01-08 14:29:53 -0600

    Scroll Text update and css updates
    css - basic layout colors and textures of dashboard
    
    js- implemented code for scroll text (still trouble shooting)

--- example/home/static/dashboard_js/example.js
+++ example/home/static/dashboard_js/example.js
@@ -2,3 +2,74 @@ function changeText(id) {
     id.innerHTML = "Ooops!";
 }
 
+$( document ).ready(function() {
+
+    console.log(test); 
+
+    // https://codepen.io/GreenSock/pen/gOOZNzQ
+// Rate = Distance over Time r=d/t
+// If we want to define the rate, and 
+// the distance is determined, 
+// time will have to be variable
+
+// We want to define the rate, and we can 
+// define that statically
+r = 100;
+adjustJank = 4; // Set this to 0 to see the jank I'm talking about ... this just adds to the distance animated to smooth out the seam
+
+// Get the initial scroll elements and save them for later
+const scrollElems = document.querySelectorAll('.scroll p');
+
+// Adjust our tween based on the object and distance given
+function adjustTween(obj, d) {
+  // Get the progress of the previous tween if it exists
+  let progress = 0;
+  if(obj.tween) {
+    progress = obj.tween.progress();
+    // Kill the previous tween
+    obj.tween.kill();
+  }
+    
+  // r = d/t 
+  // r*t = d
+  // t = d/r
+  
+  // Set the proper time
+  var t = d/r;
+
+  // Create a new tween to animate our text so that it loops
+  // Make sure to save it to the object so we can refer to it later
+  obj.tween = gsap.fromTo(obj.parentElement, {x: 0}, {
+    duration: t,
+    x: "-"+(d+adjustJank), 
+    ease: 'linear',
+    repeat: -1,
+  }).progress(progress); // Set the progress of the new tween to the same value of
+                         // the previous tween (if it exists) before it was killed
+                      
+}
+
+// Set up for what appears to be an seamless stream of text
+// This could go in an init() function
+scrollElems.forEach((obj, i) => {
+  var d = obj.offsetWidth;
+  var parent = obj.parentElement;
+  var clone = obj.cloneNode(true);
+  parent.appendChild(clone);
+  gsap.set(parent.parentElement, {width: d});
+  
+  adjustTween(obj, d);
+});
+
+// Adjust widths and tweens on resize
+window.addEventListener("resize", () => {
+  scrollElems.forEach((obj, i) => {
+    var d = obj.offsetWidth;
+    var parent = obj.parentElement;
+    gsap.set(parent.parentElement, {width: d});
+    adjustTween(obj, d);
+  });
+});
+
+
+});
