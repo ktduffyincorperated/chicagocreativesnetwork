87adede183f12763edaf76cbd9c2f0b4b8da87c1
Carlos Mercado <carlos.mercado@convr.com>
2022-03-17 18:24:52 -0400

    Static files

--- example/settings.py
+++ example/settings.py
@@ -131,10 +131,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/workspace/staticfiles',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
